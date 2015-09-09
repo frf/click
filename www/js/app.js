36efb631305879ebb4d61a02a39c0934eac244d6
frf <fabio@fabiofarias.com.br>
2015-09-09 16:42:48 -0300

    First

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
 // Ionic Starter App
-
+var fb = null;
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ngCordova','starter.controllers', 'starter.services', "firebase"])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,6 +20,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+      fb = new Firebase("https://burning-fire-921.firebaseio.com/");
+
   });
 })
 
@@ -30,16 +32,46 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-
+      .state("home", {
+          url: "/home",
+          templateUrl: "templates/home.html",
+          controller: "HomeController"
+      })
+      .state("tab.login", {
+          url: "/login",
+          cache: false,
+          views: {
+              'tab-login': {
+                  templateUrl: 'templates/login.html',
+                  controller: 'LoginController'
+              }
+          }
+      })
+      .state("tab.secure", {
+          url: "/secure",
+          views: {
+              'tab-secure': {
+                  templateUrl: 'templates/secure.html',
+                  controller: 'SecureController'
+              }
+          }
+      })
   // setup an abstract state for the tabs directive
     .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
   // Each tab has its own nav history stack:
-
+  .state("foto", {
+      url: "/foto",
+      templateUrl: "templates/foto.html",
+      controller: "FotoController"
+  }).state("addfoto", {
+      url: "/addfoto",
+      templateUrl: "templates/adicionar-foto.html",
+      controller: "imageController"
+  })
   .state('tab.dash', {
     url: '/dash',
     views: {
@@ -49,15 +81,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     }
   })
-      .state('tab.foto', {
-          url: '/foto',
-          views: {
-              'tab-foto': {
-                  templateUrl: 'templates/tab-foto.html',
-                  controller: 'FotoCtrl'
-              }
+  .state('tab.foto', {
+      url: '/foto',
+      views: {
+          'tab-foto': {
+              templateUrl: 'templates/tab-foto.html',
+              controller: 'FotoCtrl'
           }
-      })
+      }
+  })
   .state('tab.chats', {
       url: '/chats',
       views: {
@@ -76,7 +108,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
-
   .state('tab.account', {
     url: '/account',
     views: {
@@ -88,6 +119,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,219 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Chats){
-    $scope.chats = Chats.all();
-    $scope.remove = function(chat) {
-        Chats.remove(chat);
-    };
+    .controller("LoginController", function($scope, $ionicHistory, $firebaseAuth, $location) {
+
+        console.log('LOGIN');
+
+        $scope.login = function(username, password) {
+            var fbAuth = $firebaseAuth(fb);
+            fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            }).then(function(authData) {
+                $location.path("/tab/dash");
+            }).catch(function(error) {
+                console.error("ERROR: " + error);
+            });
+        }
+
+        $scope.logout = function() {
+            console.log('SAIR');
+
+            console.log(fb.unauth());
+
+            $ionicHistory.clearHistory();
+
+            $location.path("/tab/login");
+
+
+        }
+
+
+        $scope.register = function(username, password) {
+            var fbAuth = $firebaseAuth(fb);
+            fbAuth.$createUser({email: username, password: password}).then(function() {
+                return fbAuth.$authWithPassword({
+                    email: username,
+                    password: password
+                });
+            }).then(function(authData) {
+                $location.path("/tab/dash");
+            }).catch(function(error) {
+                console.error("ERROR " + error);
+            });
+        }
+
+    })
+.controller('DashCtrl', function($scope, $ionicHistory, $location, Chats){
+
+        $ionicHistory.clearHistory();
+
+        fbAuth = fb.getAuth();
+
+        if(fbAuth != null) {
+
+            $scope.chats = Chats.all();
+            $scope.remove = function (chat) {
+                Chats.remove(chat);
+            };
+
+        }else{
+            $location.path("/home");
+        }
 })
-.controller('FotoCtrl', function($scope){
+    .controller('FotoCtrl', function($scope){
+
+    }).controller('HomeController', function($scope){
+
+    }).controller('SecureController',  function($scope, $ionicHistory, $firebaseArray, $cordovaCamera) {
+
+        $ionicHistory.clearHistory();
+
+        $scope.images = [];
+
+        var fbAuth = fb.getAuth();
+        if(fbAuth) {
+            var userReference = fb.child("users/" + fbAuth.uid);
+            var syncArray = $firebaseArray(userReference.child("images"));
+            $scope.images = syncArray;
+        } else {
+            $state.go("login");
+        }
+
+        $scope.upload = function() {
+            var options = {
+                quality : 75,
+                destinationType : Camera.DestinationType.DATA_URL,
+                sourceType : Camera.PictureSourceType.CAMERA,
+                allowEdit : true,
+                encodingType: Camera.EncodingType.JPEG,
+                popoverOptions: CameraPopoverOptions,
+                targetWidth: 500,
+                targetHeight: 500,
+                saveToPhotoAlbum: false
+            };
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+                syncArray.$add({image: imageData}).then(function() {
+                    alert("Imagem enviada com sucesso");
+                });
+            }, function(error) {
+                console.error(error);
+            });
+        }
+
+    }).controller('imageController', function($scope, $cordovaCamera, $cordovaFile) {
+        // 1
+        $scope.images = [];
+
+        $scope.addImage = function() {
+            // 2
+            var options = {
+                destinationType : Camera.DestinationType.FILE_URI,
+                sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+                allowEdit : false,
+                encodingType: Camera.EncodingType.JPEG,
+                popoverOptions: CameraPopoverOptions,
+            };
+
+            // 3
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+
+                // 4
+                onImageSuccess(imageData);
+
+                function onImageSuccess(fileURI) {
+                    createFileEntry(fileURI);
+                }
+
+                function createFileEntry(fileURI) {
+                    window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+                }
+
+                // 5
+                function copyFile(fileEntry) {
+                    var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+                    var newName = makeid() + name;
+
+                    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+                            fileEntry.copyTo(
+                                fileSystem2,
+                                newName,
+                                onCopySuccess,
+                                fail
+                            );
+                        },
+                        fail);
+                }
+
+                // 6
+                function onCopySuccess(entry) {
+                    $scope.$apply(function () {
+                        $scope.images.push(entry.nativeURL);
+                    });
+                }
+
+                function fail(error) {
+                    console.log("fail: " + error.code);
+                }
+
+                function makeid() {
+                    var text = "";
+                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                    for (var i=0; i < 5; i++) {
+                        text += possible.charAt(Math.floor(Math.random() * possible.length));
+                    }
+                    return text;
+                }
+
+            }, function(err) {
+                console.log(err);
+            });
+        }
+
+        $scope.urlForImage = function(imageName) {
+            var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+            var trueOrigin = cordova.file.dataDirectory + name;
+            return trueOrigin;
+        }
+
+        $scope.sendEmail = function() {
+            // 1
+            var bodyText = "<h2>Look at this images!</h2>";
+            if (null != $scope.images) {
+                var images = [];
+                var savedImages = $scope.images;
+                for (var i = 0; i < savedImages.length; i++) {
+                    // 2
+                    images.push("" + $scope.urlForImage(savedImages[i]));
+                    // 3
+                    images[i] = images[i].replace('file://', '');
+                }
+
+                // 4
+                window.plugin.email.open({
+                        to:          ["fabio@fabiofarias.com.br"], // email addresses for TO field
+                        cc:          Array, // email addresses for CC field
+                        bcc:         Array, // email addresses for BCC field
+                        attachments: images, // file paths or base64 data streams
+                        subject:    "Just some images", // subject of the email
+                        body:       bodyText, // email body (for HTML, set isHtml to true)
+                        isHtml:    true, // indicats if the body is HTML or plain text
+                    }, function () {
+                        console.log('email view dismissed');
+                    },
+                    this);
+            }
+        }
+    }).controller("FotoController", function($scope) {
+
+        $scope.images = [];
+
+        $scope.loadImages = function() {
+            for(var i = 0; i < 100; i++) {
+                $scope.images.push({id: i, src: "http://placehold.it/50x50"});
+            }
+        }
 
 })
 .controller('ChatsCtrl', function($scope, Chats) {
